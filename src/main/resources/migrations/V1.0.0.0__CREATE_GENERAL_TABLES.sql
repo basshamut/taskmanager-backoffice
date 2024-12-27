f57b236a21b9b69f67debb461efab3137a90b333
Jesus Fajardo <es.jfajardobass@gmail.com>
2024-12-27 15:41:36 +0100

    Reset

--- src/main/resources/migrations/V1.0.0.0__CREATE_GENERAL_TABLES.sql
+++
@@ -1,8 +0,0 @@
--- Tabla de propietarios
-CREATE TABLE owners (
-    owner_id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    phone VARCHAR(15),
-    email VARCHAR(100),
-    address TEXT
-);
--- src/main/resources/migrations/V1.0.0.1__CREATE_TABLE_USERS.sql
+++
@@ -1,7 +0,0 @@
--- Tabla de usuarios
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    email VARCHAR(100) NOT NULL,
-    password VARCHAR(100) NOT NULL,
-    role VARCHAR(100) NOT NULL
-);
